65fd299cc64ea974580e1db5af6d80b1314188c8
vijith6600 <vijith4y@gmail.com>
2024-02-01 16:00:47 +0530

    Contextapi & ShopCategory implimented

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Product from './Pages/Poduct'
 import LoginSignup from './Pages/LoginSignup'
 import Footer from './Components/Footer/Footer';
 
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
+
+
 
 
 function App() {
@@ -18,9 +24,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>} />
-        <Route path="/mens" element={<ShopCategory category="mens"/>} />
-        <Route path="/womens" element={<ShopCategory category="womens"/>} />
-        <Route path="/kids" element={<ShopCategory category="kids"/>} />
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="mens"/>} />
+        <Route path="/womens" element={<ShopCategory banner={women_banner} category="womens"/>} />
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kids"/>} />
         <Route path="./Product" element={<Product/>} />
         <Route path=':productId' element={<Product/>}/>
         <Route path="/cart" element={<Cart/>} />
