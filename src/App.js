d469175c0e09d10b1c1a57a9f0815cb9ec90bc5a
vijith6600 <vijith4y@gmail.com>
2024-02-05 12:40:04 +0530

    product page error fixed with id rout display

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Cart from './Pages/Cart';
-import Product from './Pages/Poduct'
 import LoginSignup from './Pages/LoginSignup'
 import Footer from './Components/Footer/Footer';
-
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+import Product from './Pages/Product';
 
 
 
@@ -27,8 +26,9 @@ function App() {
         <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>} />
         <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>} />
         <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>} />
-        <Route path="./Product" element={<Product/>} />
-        <Route path=':productId' element={<Product/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
         <Route path="/cart" element={<Cart/>} />
         <Route path="/login" element={<LoginSignup/>} />
 
--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -5,7 +5,7 @@ import arrow_icon from '../Assets/breadcrum_arrow.png'
 const Breadcrum = (props) => {
   const {product}=props;
   return (
-    <div className='Breadcrum'>
+    <div className='breadcrum'>
       Home <img src={arrow_icon} alt="Arrow" /> SHOP <img src={arrow_icon} alt="Arrow" /> { product.category} <img src={arrow_icon} alt="Arrow"/>{product.name}
     </div>
   )
