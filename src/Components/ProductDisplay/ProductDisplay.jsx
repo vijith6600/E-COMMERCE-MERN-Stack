bbe19e5952b3f22553742415866369ee2be0b0c5
vijith6600 <vijith4y@gmail.com>
2024-02-13 22:17:46 +0530

    cart context logic updated

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const {addToCart} = useContext(ShopContext)
   return (
     <div className="producrdisplay">
       <div className="productdisplay-left">
@@ -53,7 +55,7 @@ const ProductDisplay = (props) => {
             <div>XXL</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
         <p className="productdisplay-right-category">
           <span>Category</span> Women , T-Shirt, Crop Top
         </p>
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,10 +1,33 @@
-import React, { createContext } from "react";
+import React, { createContext , useState } from "react";
 import  all_product from '../Components/Assets/all_product';
 
 export const ShopContext = createContext(null);
 
+
+const getDefaulfCart = ()=>{
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++) {
+
+        cart[index]=0;
+    }
+    return cart;
+}
+
 const ShopContextProvider =(props)=>{
-    const contextValue = {all_product}
+    const [cartItem,setCartItems]= useState(getDefaulfCart());
+
+
+
+    const addToCart = (itemId)=>{
+        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] +1}))
+        console.log(cartItem);
+    }
+
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] -1}))
+    }
+
+    const contextValue = {all_product,cartItem,addToCart,removeFromCart}
 
     return (
         <ShopContext.Provider value={contextValue}>
