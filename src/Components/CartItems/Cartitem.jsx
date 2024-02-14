2736687e68cf091cef555ef9aa96401867b4d88f
vijith6600 <vijith4y@gmail.com>
2024-02-14 22:23:01 +0530

    edited

--- src/Components/CartItems/Cartitem.jsx
+++ src/Components/CartItems/Cartitem.jsx
@@ -1,37 +1,47 @@
-import React, { useContext } from 'react'
-import './Cartitem.css'
-import { ShopContext } from '../../Context/ShopContext'
-import remove_icon from '../Assets/cart_cross_icon.png'
+import React, { useContext } from "react";
+import "./Cartitem.css";
+import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const Cartitem = () => {
-    const {all_product,Cartitem,removeFromCart} = useContext(ShopContext);
-
+  const { all_product, Cartitem, removeFromCart } = useContext(ShopContext);
 
   return (
-    <div className='cartitems'>
-        <div className="cartitems-format-main">
-            <p>Produce</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
-        </div>
-        <hr />
-        <div>
-            <div className="cartitems-format">
-                <img src="" alt="" className="cart-product-icon" />
-                <p></p>
-                <p></p>
-                <button></button>
-                <p></p>
-                <img src="remove_icon" onClick={()=>{removeFromCart()}} alt="" />
-               
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Produce</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (Cartitem[e.id] > 0) {
+          return 
+            <div>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="cart-product-icon" />
+                <p>{e.name}</p>
+                <p>{e.new_price}</p>
+                <button className="cartitems-quantity">{Cartitem[e.id]}</button>
+                <p>{e.new_price * Cartitem[e.id]}</p>
+                <img
+                  src="remove_icon"
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+              <hr />
             </div>
-            <hr />
-        </div>
+          
+        }
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Cartitem
+export default Cartitem;
