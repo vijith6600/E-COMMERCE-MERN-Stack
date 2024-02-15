ff9f9599d4a18624be57cb060ec798a3fdff55ba
vijith6600 <vijith4y@gmail.com>
2024-02-15 12:37:36 +0530

    cart page design done

--- src/Components/CartItems/Cartitem.jsx
+++ src/Components/CartItems/Cartitem.jsx
@@ -20,13 +20,13 @@ const Cartitem = () => {
       {all_product.map((e) => {
         if (cartItem[e.id] > 0) {
           return  <div>
-              <div className="cartitems-format">
+              <div className="cartitems-format cartitems-format-main ">
                 <img src={e.image} alt="" className="cart-product-icon" />
                 <p>{e.name}</p>
-                <p>{e.new_price}</p>
+                <p>${e.new_price}</p>
                 <button className="cartitems-quantity">{cartItem[e.id]}</button>
-                <p>{e.new_price * cartItem[e.id]}</p>
-                <img
+                <p>${e.new_price * cartItem[e.id]}</p>
+                <img className="cart-remove-icon"
                   src={remove_icon}
                   onClick={() => {
                     removeFromCart(e.id);
@@ -38,7 +38,37 @@ const Cartitem = () => {
             </div>
           
         }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>cart Total</h1>
+          <div>
+            <div className="cart-item-total-item">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>${0}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promocode enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
