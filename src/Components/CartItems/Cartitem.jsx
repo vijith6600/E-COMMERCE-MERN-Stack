be84e3fe9380897dc86d454c867129d0cdb9e4fb
vijith6600 <vijith4y@gmail.com>
2024-02-15 08:03:29 +0530

    cart page error fix

--- src/Components/CartItems/Cartitem.jsx
+++ src/Components/CartItems/Cartitem.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 
 const Cartitem = () => {
-  const { all_product, Cartitem, removeFromCart } = useContext(ShopContext);
+  const { all_product, cartItem, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className="cartitems">
@@ -18,17 +18,16 @@ const Cartitem = () => {
       </div>
       <hr />
       {all_product.map((e) => {
-        if (Cartitem[e.id] > 0) {
-          return 
-            <div>
+        if (cartItem[e.id] > 0) {
+          return  <div>
               <div className="cartitems-format">
                 <img src={e.image} alt="" className="cart-product-icon" />
                 <p>{e.name}</p>
                 <p>{e.new_price}</p>
-                <button className="cartitems-quantity">{Cartitem[e.id]}</button>
-                <p>{e.new_price * Cartitem[e.id]}</p>
+                <button className="cartitems-quantity">{cartItem[e.id]}</button>
+                <p>{e.new_price * cartItem[e.id]}</p>
                 <img
-                  src="remove_icon"
+                  src={remove_icon}
                   onClick={() => {
                     removeFromCart(e.id);
                   }}
